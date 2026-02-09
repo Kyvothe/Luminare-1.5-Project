349836daf79010ef6e9e432fd55dc2c5f1dd25e0
FelicitasSchrempp <f_schrempp@gmx.de>
2026-02-09 18:01:51 +0100

    hedgehog almost there

--- Assets/+++ Workdata/Traps/Spikes/SpikesRise.cs
+++ Assets/+++ Workdata/Traps/Spikes/SpikesRise.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class SpikesRise : MonoBehaviour
 {
-    
+    private Animator _animator;
     BoxCollider2D _boxCollider2D;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             transform.position = new Vector2(transform.position.x, transform.position.y + 0.3f);
             //gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            
+            _animator.SetBool("Angry", true);
+            
+            Debug.Log("Hello");
         }
     }
     
@@ -19,6 +30,11 @@ public class SpikesRise : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x, transform.position.y - 0.3f);
             //gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            
+            _animator.SetBool("Angry", false);
+
         }
+        
+        
     }
 }
