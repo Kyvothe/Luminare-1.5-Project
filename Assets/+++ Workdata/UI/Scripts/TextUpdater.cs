226951f7dded04c8414a9161bbf32256880b650a
FelicitasSchrempp <f_schrempp@gmx.de>
2026-02-13 14:09:02 +0100

    mini fix

--- Assets/+++ Workdata/UI/Scripts/TextUpdater.cs
+++ Assets/+++ Workdata/UI/Scripts/TextUpdater.cs
@@ -6,17 +6,17 @@ public class TextUpdater : MonoBehaviour
 {
     public GameObject player;
    
-    private PlayerZoneCheck playerZoneCheck;
+    private PlayerZoneCheck _playerZoneCheck;
     private int _itemCount;
 
     private void Awake()
     {
-        playerZoneCheck = player.GetComponent<PlayerZoneCheck>();
+        _playerZoneCheck = player.GetComponent<PlayerZoneCheck>();
     }
 
     private void FixedUpdate()
     {
-        _itemCount = playerZoneCheck.ReturnItemCount();                                                                 // Zugriff auf item count über Methode mit Rückgabewert
+        _itemCount = _playerZoneCheck.ReturnItemCount();                                                                 // Zugriff auf item count über Methode mit Rückgabewert
         gameObject.GetComponent<TMP_Text>().text = $"x {_itemCount}/10";
     }
 }
