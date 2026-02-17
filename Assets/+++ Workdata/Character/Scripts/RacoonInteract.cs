7ab86a6db6d25359500abd75a0e68c39cecae075
FelicitasSchrempp <f_schrempp@gmx.de>
2026-02-17 18:03:48 +0100

    ratgang not working yet

---
+++ Assets/+++ Workdata/Character/Scripts/RacoonInteract.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using Random = System.Random;
+
+public class RacoonInteract : MonoBehaviour
+{
+    public static readonly int Hash_ActionId = Animator.StringToHash("ActionId");
+    public static readonly int Hash_ActionTrigger = Animator.StringToHash("ActionTrigger");
+    
+    private Animator _anim;
+
+    public GameObject player;
+    private PlayerZoneCheck _playerZoneCheck;
+
+    private bool _isTalking = false;
+
+    private float _random = 0f;
+
+    private void Awake()
+    {
+        _anim = gameObject.GetComponent<Animator>();
+
+        _playerZoneCheck = player.GetComponent<PlayerZoneCheck>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_random == 2 )
+        {
+            _anim.SetInteger(Hash_ActionId, 3);
+            _anim.SetTrigger(Hash_ActionTrigger);
+
+            _random = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Hi");
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _anim.SetInteger(Hash_ActionId, 0);
+
+            _isTalking = false;
+        }
+    }
+
+    public void StartDialogue()
+    { 
+        _anim.SetInteger(Hash_ActionId, 1);
+        _anim.SetTrigger(Hash_ActionTrigger);
+
+        _isTalking = true;
+
+        StartCoroutine(RandomDiva());
+        
+        Debug.Log("Fix Wing"); // Dialog einbauen belohnung
+        player.GetComponent<PlayerController>().SetCanFly(true);
+    }
+
+    private IEnumerator RandomDiva()
+    {
+        while (_isTalking)
+        {
+            yield return new WaitForSeconds(2f);
+            _random = UnityEngine.Random.Range(1,3);
+        }
+    }
+}
