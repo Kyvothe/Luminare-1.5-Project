5beb8de138bd2b95bfa678c1400c9e843a4d7407
FelicitasSchrempp <f_schrempp@gmx.de>
2026-02-19 15:47:07 +0100

    attempt button fix prestory

--- Assets/+++ Workdata/UI/Scripts/PreStory_UIManager.cs
+++ Assets/+++ Workdata/UI/Scripts/PreStory_UIManager.cs
@@ -9,6 +9,12 @@ public class PreStory_UIManager : MonoBehaviour
 
     private GameObject _currentMenu;
 
+    public GameObject Button1;
+    public GameObject Button2;
+    public GameObject Button3;
+    public GameObject Button4;
+    public GameObject Button5;
+
     private int _count;
 
     private void Awake()
@@ -21,7 +27,32 @@ public class PreStory_UIManager : MonoBehaviour
     {
         if (Keyboard.current.anyKey.wasPressedThisFrame && EventSystem.current.currentSelectedGameObject == null)
         { 
-            EventSystem.current.SetSelectedGameObject(_currentMenu.GetComponent<DefaultButtonSetter>().ReturnButton());
+            //EventSystem.current.SetSelectedGameObject(_currentMenu.GetComponent<DefaultButtonSetter>().ReturnButton());
+
+            if (_count == 0)
+            {
+                EventSystem.current.SetSelectedGameObject(Button1);
+            }
+            
+            if (_count == 1)
+            {
+                EventSystem.current.SetSelectedGameObject(Button2);
+            }
+            
+            if (_count == 2)
+            {
+                EventSystem.current.SetSelectedGameObject(Button3);
+            }
+            
+            if (_count == 3)
+            {
+                EventSystem.current.SetSelectedGameObject(Button4);
+            }
+            
+            if (_count == 4)
+            {
+                EventSystem.current.SetSelectedGameObject(Button5);
+            }
         }
     }
 
