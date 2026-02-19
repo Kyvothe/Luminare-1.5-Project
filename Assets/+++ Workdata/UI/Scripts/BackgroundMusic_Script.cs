0166de6554fb6b931375b7ef7cce388a98386ece
Kyvothe <lars-wilken@gmx.net>
2026-02-19 16:46:10 +0100

    Music Manager
    Music Manager added
    Music sorted

---
+++ Assets/+++ Workdata/UI/Scripts/BackgroundMusic_Script.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BackgroundMusic_Script : MonoBehaviour
+{
+    public static BackgroundMusic_Script instance;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
