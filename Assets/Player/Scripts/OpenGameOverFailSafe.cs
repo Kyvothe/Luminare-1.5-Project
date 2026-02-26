df2e9fb5a9d9c847592336b4774e0e3563529cf2
FelicitasSchrempp <f_schrempp@gmx.de>
2026-02-26 13:19:22 +0100

    fail safe for death

---
+++ Assets/Player/Scripts/OpenGameOverFailSafe.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OpenGameOverFailSafe : MonoBehaviour
+{
+    // will ensure the gameover screen opens in case the death animation gets overwritten
+
+    public GameObject MenuManagerInGame;
+
+    private int _health;
+    private PlayerInformation _playerInformation;
+
+    private void Awake()
+    {
+        _playerInformation = GetComponent<PlayerInformation>();
+    }
+
+    private void FixedUpdate()
+    {
+        _health = _playerInformation.ReturnHealth();
+
+        if (_health <= 0)
+        {
+            StartCoroutine(ReallyGameOver());
+        }
+    }
+
+    private IEnumerator ReallyGameOver()
+    {
+        yield return new WaitForSeconds(4f);
+        MenuManagerInGame.GetComponent<OpenDialogueInGame>().OpenGameOverScreen();
+    }
+}
